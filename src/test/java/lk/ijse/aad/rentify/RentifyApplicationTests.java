c875e1c0dd3dae05b2f7d837e66a66a706dbcbb8
saumy <saumyadivyanjalee529@gmail.com>
2026-03-01 21:03:32 +0530

    first commit

---
+++ src/test/java/lk/ijse/aad/rentify/RentifyApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.aad.rentify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentifyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
