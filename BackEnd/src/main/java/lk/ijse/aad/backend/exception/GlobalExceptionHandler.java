3c14b7d7685abaaab3ac757705579cf14dc0425e
saumy <saumyadivyanjalee529@gmail.com>
2026-03-11 18:11:50 +0530

    sign-in and sign-up

--- BackEnd/src/main/java/lk/ijse/aad/backend/exception/GlobalExceptionHandler.java
+++ BackEnd/src/main/java/lk/ijse/aad/backend/exception/GlobalExceptionHandler.java
@@ -1,5 +1,39 @@
 package lk.ijse.aad.backend.exception;
 
+import lk.ijse.aad.backend.dto.APIResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public ResponseEntity<APIResponse> handleUserNotFound(UsernameNotFoundException ex){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                .body(new APIResponse(404, ex.getMessage(), null));
+    }
+
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<APIResponse> handleBadCredentials(BadCredentialsException ex){
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                .body(new APIResponse(401, ex.getMessage(), null));
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<APIResponse> handleRuntime(RuntimeException ex){
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                .body(new APIResponse(400, ex.getMessage(), null));
+
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<APIResponse> handleGeneral(Exception ex){
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new APIResponse(500,"Something went wrong: "+ ex.getMessage(), null));
+    }
 
 }
